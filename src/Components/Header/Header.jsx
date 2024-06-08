4d32b10271cbd3acea779f009c9b13446627b07c
Dhiraj Bhawsar <dhirajbhavsar.official@gmail.com>
2024-06-08 20:52:38 +0530

    dp0ojem

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,38 @@
+import { useState } from 'react';
 import './Header.css';
 import Logo from './../../assets/images/amazon-png-logo-vector-6701.png';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-
+import CloseIcon from '@mui/icons-material/Close';
 
 const Header = () => {
+    const menuItems = [
+        { id: 1, text: 'Amazon MinTV' },
+        { id: 2, text: 'Sell' },
+        { id: 3, text: 'Best Sellers' },
+        { id: 4, text: 'Mobiles' },
+        { id: 5, text: 'Todays Deal' },
+        { id: 6, text: 'Fashion' },
+        { id: 7, text: 'Electronics' },
+        { id: 8, text: 'Prime' },
+        { id: 9, text: 'Home kitchen' },
+        { id: 10, text: 'Customer Service' },
+        { id: 11, text: 'Amazon Pay' },
+        { id: 12, text: 'Computers' },
+        { id: 13, text: 'Books' },
+        { id: 14, text: 'Car & Motorbikes' },
+        { id: 15, text: 'Gift' },
+        { id: 18, text: 'Sports fitness And OutDoors' },
+    ];
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+        console.log(isMenuOpen ? 'Menu is open' : 'Menu is closed');
+    };
+
     return (
         <>
             <nav className="bg-[#131921] flex flex-wrap items-center p-2">
@@ -13,16 +40,6 @@ const Header = () => {
                     <div className="flex items-center cursor-pointer mx-2">
                         <img className="w-[80px] sm:w-[100px] md:w-[110px]" src={Logo} alt="Amazon Logo" />
                         <span className="text-white text-center hidden md:inline">.in</span>
-                    </div>
-                    <div className="flex md:hidden items-center">
-                        <div className="flex flex-col cursor-pointer mx-2">
-                            <span className="text-gray-200 font-semibold text-[10px] sm:text-[12px] md:text-[14px]">Hello, sign in</span>
-                            <span className="text-white font-semibold text-[13px] sm:text-[15px] md:text-[17px]">Account and Lists</span>
-                        </div>
-                        <div className="flex items-center cursor-pointer mx-2">
-                            <ShoppingCartOutlinedIcon style={{ fill: 'white', width: '30px', height: '35px' }} />
-                            <span className="text-white font-semibold text-[13px] sm:text-[15px] md:text-[17px]">Cart</span>
-                        </div>
                     </div>
                     <div className="hidden md:flex flex-col justify-center items-center px-2 cursor-pointer">
                         <span className="text-gray-300 font-bold text-[10px] sm:text-[12px] md:text-[14px] ml-3">Delivery To Nashik</span>
@@ -35,9 +52,8 @@ const Header = () => {
 
                 <div className="flex-grow mx-2 py-3 px-1 flex justify-center lg:justify-start">
                     <form action="search" className="flex justify-center items-center w-full">
-                        {/* Conditional rendering for select dropdown */}
-                        <div className=" bg-[#D4D4D4] cursor-pointer md:block hidden">
-                            <select name="" id="" className="bg-[#D4D4D4] rounded-l-sm p-2 w-12 sm:w-14 md:w-16 outline-none">
+                        <div className="bg-[#D4D4D4] cursor-pointer hidden md:block">
+                            <select className="bg-[#D4D4D4] rounded-l-sm p-2 w-12 sm:w-14 md:w-16 outline-none">
                                 <option value="">All</option>
                                 <option value="">Mobiles</option>
                                 <option value="">Laptops</option>
@@ -65,21 +81,49 @@ const Header = () => {
                         <span className="text-white font-semibold text-[13px] sm:text-[15px] md:text-[17px]">Cart</span>
                     </div>
                 </div>
+
+                <div className="md:hidden flex items-center">
+                    <div className="flex flex-col cursor-pointer mx-2">
+                        <span className="text-gray-200 font-semibold text-[10px] sm:text-[12px]">Hello, sign in</span>
+                        <span className="text-white font-semibold text-[13px] sm:text-[15px]">Account and Lists</span>
+                    </div>
+                    <div className="flex items-center cursor-pointer mx-2">
+                        <ShoppingCartOutlinedIcon style={{ fill: 'white', width: '30px', height: '35px' }} />
+                        <span className="text-white font-semibold text-[13px] sm:text-[15px]">Cart</span>
+                    </div>
+                </div>
             </nav>
 
-            <section className='bg-[#232F3E]'>
-                <div className='flex text-white justify-center items-center  '>
-                    <button className="text-white focus:outline-none p-3 m-1">
-                        <span className="block h-1 w-6 bg-white mb-1"></span>
-                        <span className="block h-1 w-6 bg-white mb-1"></span>
-                        <span className="block h-1 w-6 bg-white"></span>
+            <section className="bg-[#232F3E]">
+                <div className="flex items-center text-white p-2">
+                    <button onClick={toggleMenu} className="text-white focus:outline-none p-2 m-1 menu-button">
+                        {isMenuOpen ? <CloseIcon /> : (
+                            <>
+                                <span className="block h-0.5 w-5 bg-white mb-1"></span>
+                                <span className="block h-0.5 w-5 bg-white mb-1"></span>
+                                <span className="block h-0.5 w-5 bg-white"></span>
+                            </>
+                        )}
                     </button>
-                    <span>All</span>
+                    <div className="hidden md:flex">
+                    {menuItems.map((item) => (
+                        <div key={item.id} className="px-4 py-2">
+                            <span className="text-sm text-white">{item.text}</span>
+                        </div>
+                    ))}
+                    </div>
                 </div>
-                
+
+                <div className={`menu-slide-in  ${isMenuOpen ? 'open' : ''}`}>
+                    {menuItems.map((item) => (
+                        <div key={item.id} className="px-4 py-2 border-t border-gray-700">
+                            <span className="text-sm text-white">{item.text}</span>
+                        </div>
+                    ))}
+                </div>
             </section>
         </>
-    )
-}
+    );
+};
 
 export default Header;
